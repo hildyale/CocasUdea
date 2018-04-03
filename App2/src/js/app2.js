3126e1d671b8d213126ad1a4b97088efb1465a0b
sugar <sebastian.gbotero@gmail.com>
2018-04-02 22:09:34 -0500

    Parte Sebas

---
+++ App2/src/js/app2.js
@@ -0,0 +1,81 @@
+//Leche, Lácteos 
+function loadLecheLacteos() {        
+    $("#foods").empty();
+    $(".container").append(
+            "<div class='item'><img src='src/images/food/png/017-milk.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/039-cheese.png' width='130' height='100' /></div>"
+            );
+    
+}
+
+//Cereales, Legumbres
+    function loadCerealesLegumbresFrutos() {
+    $("#foods").empty();    
+    $(".container").append(
+         "<div class='item'><img src='src/images/food/png/022-rice.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/010-croissant.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/023-taco.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/028-bread.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/032-noodles.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/041-potato.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/010-corn.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/013-peas.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/beans.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/cereals.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/flour.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/grain.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/pasta.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/seeds.png' width='130' height='100' /></div>"
+        );
+}
+      
+    
+//Aceites, Grasa origen animal
+    function loadAceitesGrasas() {
+    $("#foods").empty();
+        $(".container").append(
+            "<div class='item'><img src='src/images/food/png/033-butter.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/oil.png' width='130' height='100' /></div>"
+            );
+        
+}
+    
+//Frutas, Verduras
+   function loadFrutasVerduras() {
+   $("#foods").empty();
+       $(".container").append(
+         "<div class='item'><img src='src/images/food/png/037-turnip.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/038-aubergine.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/040-garlic.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/042-cabbage.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/043-avocado.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/044-tomato.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/045-onion.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/046-carrot.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/047-pepper.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/048-pumpkin.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/049-cherry.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/002-orange.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/003-apple.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/004-pear.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/001-grape.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/004-bell-pepper.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/005-melon-1.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/006-lemon.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/008-orange-1.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/009-papaya.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/014-berries.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/015-grenade.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/016-artichoke.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/017-cabbage.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/018-coconut.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/019-watermelon-1.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/021-lime.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/022-potato.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/023-kiwi.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/024-lettuce-1.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/025-pumpkin.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/028-banana.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/030-plum.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/031-beet.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/033-cucumber.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/034-strawberry.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/035-mushroom-1.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/036-mango.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/037-pineapple.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/038-chili.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/040-radish.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/043-eggplant.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/045-broccoli.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/048-cherry.png' width='130' height='100' /></div> <div class='item'><img src='src/images/fruits-vegetables/png/049-raspberry.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/blueberries.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/cauliflower.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/chives.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/fig.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/hazelnut.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/olives.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/peach.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/pepper.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/pistachio.png' width='130' height='100' /></div>"
+         
+         );
+       
+} 
+    
+//Carnes, Pescados, Huevos
+    function loadCarnesPescadosHuevos() {
+    $("#foods").empty();
+    $(".container").append(
+         "<div class='item'><img src='src/images/food/png/009-squid.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/011-steak.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/012-tuna.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/019-sausage.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/025-hotdog.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/027-fish.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/029-chicken.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/030-egg-1.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/031-sushi.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/bacon.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/ham.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/kebab-2.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/meat.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/octopus.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/risotto.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/salmon.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/salami.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/shrimp.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/sushi-1.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/turkey.png' width='130' height='100' /></div>"
+         
+         ); 
+}
+    
+//Azúcarados, Pastelería
+    function loadAzucaradosPasteleria() {
+    $("#foods").empty();
+     $(".container").append(
+         "<div class='item'><img src='src/images/food/png/006-pizza.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/008-soft-drink.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/013-wine.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/014-juice.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/015-beer.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/016-tea.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/020-cake.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/021-chocolate.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/024-burger.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/026-fries.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/034-cookie.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/035-jam.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/036-ice-cream.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/biscuit.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/butter.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/candy.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/chips.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/cookies.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/cupcake.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/doughnut.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/honey.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/jelly.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/pancakes.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/pretzel.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/pudding.png' width='130' height='100' /></div> <div class='item'><img src='src/images/gastronomy/png/coffee.png' width='130' height='100' /></div>"
+         
+         );
+}
+
+$(document).ready(function() {
+ var options = {minMargin: 5, maxMargin: 15, itemSelector: ".item", firstItemClass: "first-item"};
+ $(".container").rowGrid(options);
+    $(".container").append(
+     "<div class='item'><img src='src/images/food/png/001-grape.png' width='130' height='100' /></div>     <div class='item'> <img src='src/images/food/png/002-orange.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/003-apple.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/004-pear.png' width='130' height='100' /> </div> <div class='item'><img src='src/images/food/png/005-vinegar.png' width='130' height='100' /> </div> <div class='item'><img src='src/images/food/png/006-pizza.png' width='130' height='100' /> </div> <div class='item'><img src='src/images/food/png/007-rice-1.png' width='130' height='100' /> </div> <div class='item'><img src='src/images/food/png/008-soft-drink.png' width='130' height='100' /></div> <div class='item'><img src='src/images/food/png/009-squid.png' width='130' height='100' /></div>"                      
+                       
+);
+
+
+
+ 
+// endless scrolling
+ /*$(window).scroll(function() {
+if($(window).scrollTop() + $(window).height() == $(document).height()) {
+ $(".container").append(
+     
+                        
+                         
+
+ 
+);
+    
+$(".container").rowGrid("appended");
+ }
+ /*});*/
+});
